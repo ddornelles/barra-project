a6651e28600bb5d7358599bb15062012f0012798
Uira Oliveira <uira.sampaio@hotmail.com>
2019-07-13 15:15:22 -0300

    another test

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 const passport = require('passport');
+console.log('teste')
 
 
 mongoose
@@ -29,7 +30,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser())
 
 // Express View engine setup
 app.set('views', path.join(__dirname, 'views'));
