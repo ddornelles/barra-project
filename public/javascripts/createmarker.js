93c13ab1b40af41e725a69974deffd8ed6c2806f
David Dornelles <daviddornelles@gmail.com>
2019-07-16 10:43:25 -0300

    seach beaches (2nd screen) working w/ display result name

--- public/javascripts/createmarker.js
+++ public/javascripts/createmarker.js
@@ -1,91 +1,91 @@
 function initAutocomplete() {
 
   document.getElementById('button-search').onclick = function () {
+    const input = document.getElementById('pac-input');
+
+    google.maps.event.trigger(input, 'focus', {});
+    google.maps.event.trigger(input, 'keydown', { keyCode: 13 });
+    google.maps.event.trigger(this, 'focus', {});
+
+  };
+
+  const map = new google.maps.Map(document.getElementById('searchmap'), {
+    center: { lat: -22.9644821, lng: -43.2308992 },
+    zoom: 13,
+    mapTypeId: 'roadmap',
+  });
+
+  // Create the search box and link it to the UI element.
   const input = document.getElementById('pac-input');
+  const searchBox = new google.maps.places.SearchBox(input);
 
-  google.maps.event.trigger(input, 'focus', {});
-  google.maps.event.trigger(input, 'keydown', { keyCode: 13 });
-  google.maps.event.trigger(this, 'focus', {});
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', () => {
+    searchBox.setBounds(map.getBounds());
+  });
 
-};
+  let markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', () => {
+    const places = searchBox.getPlaces();
 
- const map = new google.maps.Map(document.getElementById('searchmap'), {
-   center: { lat: -22.9644821, lng: -43.2308992 },
-   zoom: 13,
-   mapTypeId: 'roadmap',
- });
+    if (places.length === 0) {
+      return;
+    }
 
- // Create the search box and link it to the UI element.
- const input = document.getElementById('pac-input');
- const searchBox = new google.maps.places.SearchBox(input);
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
 
- // Bias the SearchBox results towards current map's viewport.
- map.addListener('bounds_changed', () => {
-   searchBox.setBounds(map.getBounds());
- });
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach((place) => {
+      if (!place.geometry) {
+        console.log('Returned place contains no geometry');
+        return;
+      }
+      document.getElementById('search-result').innerHTML = place.formatted_address;
+      // Create a marker for each place.
 
- let markers = [];
- // Listen for the event fired when the user selects a prediction and retrieve
- // more details for that place.
- searchBox.addListener('places_changed', () => {
-   const places = searchBox.getPlaces();
+      console.log(place.geometry.location.lat());
+      console.log(place.geometry.location.lng());
 
-   if (places.length === 0) {
-     return;
-   }
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 
-   // Clear out the old markers.
-   markers.forEach((marker) => {
-     marker.setMap(null);
-   });
-   markers = [];
+  const getBarracas = () => {
+    axios.get('/api')
+      .then((response) => {
+        placeBarracas(response.data.barracas);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-   // For each place, get the icon, name and location.
-   const bounds = new google.maps.LatLngBounds();
-   places.forEach((place) => {
-     if (!place.geometry) {
-       console.log('Returned place contains no geometry');
-       return;
-     }
+  const placeBarracas = (barracas) => {
+    barracas.forEach((barraca) => {
+      const center = {
+        lat: barraca.location.coordinates[1],
+        lng: barraca.location.coordinates[0],
+      };
+      const pin = new google.maps.Marker({
+        position: center,
+        map,
+        title: barraca.name,
+      });
+    });
+  };
 
-     // Create a marker for each place.
-
-     console.log(place.geometry.location.lat());
-     console.log(place.geometry.location.lng());
-
-     if (place.geometry.viewport) {
-       // Only geocodes have viewport.
-       bounds.union(place.geometry.viewport);
-     } else {
-       bounds.extend(place.geometry.location);
-     }
-   });
-   map.fitBounds(bounds);
- });
-
- const getBarracas = () => {
-   axios.get('/api')
-     .then((response) => {
-       placeBarracas(response.data.barracas);
-     })
-     .catch((error) => {
-       console.log(error);
-     });
- };
-
- const placeBarracas = (barracas) => {
-   barracas.forEach((barraca) => {
-     const center = {
-       lat: barraca.location.coordinates[1],
-       lng: barraca.location.coordinates[0],
-     };
-     const pin = new google.maps.Marker({
-       position: center,
-       map,
-       title: barraca.name,
-     });
-   });
- };
-
- getBarracas();
+  getBarracas();
 }
