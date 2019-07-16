96876c0cd6fce351a5bd1c58d11c156d16477d1b
David Dornelles <daviddornelles@gmail.com>
2019-07-15 21:51:40 -0300

    map buscando com markers

--- public/javascripts/_createmarker.js
+++ public/javascripts/_createmarker.js
@@ -1,59 +1,44 @@
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('searchmap'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
-    mapTypeId: 'roadmap'
+function searchMap() {
+  // Initialize the map
+  const map = new google.maps.Map(document.getElementById('searchmap'), {
+    center: {lat: -22.9732303, lng: -43.2033079},
+    zoom: 14,
+    mapTypeId: 'roadmap',
   });
- 
+
   // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
- 
+  const input = document.getElementById('search-home');
+  const searchBox = new google.maps.places.SearchBox(input);
+
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
+  map.addListener('bounds_changed', function () {
     searchBox.setBounds(map.getBounds());
   });
- 
+
   var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
+  searchBox.addListener('places_changed', function () {
     var places = searchBox.getPlaces();
- 
+
     if (places.length == 0) {
       return;
     }
- 
+
     // Clear out the old markers.
-    markers.forEach(function(marker) {
+    markers.forEach(function (marker) {
       marker.setMap(null);
     });
     markers = [];
- 
+
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
+    places.forEach(function (place) {
       if (!place.geometry) {
         console.log("Returned place contains no geometry");
         return;
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
- 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
- 
+
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
@@ -89,4 +74,7 @@ function initAutocomplete() {
   };
 
   getBarracas();
- }
+
+}
+
+searchMap();
